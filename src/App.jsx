d86baa8a085022bd2ea072562fb8527f2a7a7f01
itzBaowy <notbao.js@gmail.com>
2025-09-22 21:41:34 +0700

    add Services, Features, Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './styles/App.css'
 import Header from './components/layout/Header'
 import Hero from './components/sections/Hero'
+import Services from './components/sections/Services'
+import Features from './components/sections/Features'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
+      <Services />
+      <Features />
+      <Footer />
     </div>
   )
 }
