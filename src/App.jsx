4f58794da075b44e897a10de5d55e57c9583f1c4
itzBaowy <notbao.js@gmail.com>
2025-11-11 13:27:42 +0700

    fix: customer can now redirect to paymentSuccess route

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,102 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './styles/App.css'
-import LandingPage from './pages/LandingPage'
-import Login from './components/auth/Login'
-import ProtectedRoute from './components/auth/ProtectedRoute'
-import AdminDashboard from './pages/admin/AdminDashboard'
-import CustomerDashboard from './pages/customer/CustomerDashboard'
-import StaffDashboard from './pages/staff/StaffDashboard'
-import TechnicianDashboard from './pages/technician/TechnicianDashboard'
-import Contact from './pages/customer/Contact'
-import PaymentSuccess from './pages/customer/PaymentSuccess'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./styles/App.css";
+import LandingPage from "./pages/LandingPage";
+import Login from "./components/auth/Login";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import CustomerDashboard from "./pages/customer/CustomerDashboard";
+import StaffDashboard from "./pages/staff/StaffDashboard";
+import TechnicianDashboard from "./pages/technician/TechnicianDashboard";
+import Contact from "./pages/customer/Contact";
+import PaymentSuccess from "./pages/customer/PaymentSuccess";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route 
-            path="/" 
+          <Route
+            path="/"
             element={
               <ProtectedRoute requireAuth={false}>
                 <LandingPage />
               </ProtectedRoute>
-            } 
+            }
           />
-          <Route 
-            path="/login" 
+          <Route
+            path="/login"
             element={
               <ProtectedRoute requireAuth={false}>
                 <Login />
               </ProtectedRoute>
-            } 
+            }
           />
-          <Route 
-            path="/admin" 
+          <Route
+            path="/admin"
             element={
-              <ProtectedRoute requireAuth={true} allowedRoles={['ROLE_ADMIN']}>
+              <ProtectedRoute requireAuth={true} allowedRoles={["ROLE_ADMIN"]}>
                 <AdminDashboard />
               </ProtectedRoute>
-            } 
+            }
           />
-          <Route 
-            path="/customer" 
+          <Route
+            path="/customer"
             element={
-              <ProtectedRoute requireAuth={true} allowedRoles={['ROLE_CUSTOMER']}>
+              <ProtectedRoute
+                requireAuth={true}
+                allowedRoles={["ROLE_CUSTOMER"]}
+              >
                 <CustomerDashboard />
               </ProtectedRoute>
-            } 
+            }
           />
-          <Route 
-            path="/staff" 
+          <Route
+            path="/staff"
             element={
-              <ProtectedRoute requireAuth={true} allowedRoles={['ROLE_STAFF']}>
+              <ProtectedRoute requireAuth={true} allowedRoles={["ROLE_STAFF"]}>
                 <StaffDashboard />
               </ProtectedRoute>
-            } 
+            }
           />
-          <Route 
-            path="/technician" 
+          <Route
+            path="/technician"
             element={
-              <ProtectedRoute requireAuth={true} allowedRoles={['ROLE_TECHNICIAN']}>
+              <ProtectedRoute
+                requireAuth={true}
+                allowedRoles={["ROLE_TECHNICIAN"]}
+              >
                 <TechnicianDashboard />
               </ProtectedRoute>
-            } 
+            }
           />
-          <Route 
-            path="/contact" 
+          <Route
+            path="/contact"
             element={
-              <ProtectedRoute requireAuth={true} allowedRoles={['ROLE_CUSTOMER']}>
+              <ProtectedRoute
+                requireAuth={true}
+                allowedRoles={["ROLE_CUSTOMER"]}
+              >
                 <Contact />
               </ProtectedRoute>
-            } 
+            }
           />
-          <Route 
-            path="/paymentSuccess" 
+          <Route
+            path="/paymentSuccess"
             element={
-              <ProtectedRoute requireAuth={false}>
+              <ProtectedRoute
+                requireAuth={true}
+                allowedRoles={["ROLE_CUSTOMER"]}
+              >
                 <PaymentSuccess />
               </ProtectedRoute>
-            } 
+            }
           />
-          
+
           {/* TODO: Add more routes for forgot-password, etc. */}
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
